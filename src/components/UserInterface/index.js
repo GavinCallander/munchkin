788bd1837b5e96a4ad9a409569e6021e9408d3b8
Gavin Callander <gavincallander@MacBook-Pro.local>
2021-09-22 12:05:05 -0400

    UserInterface basic set up complete with conditional rendering in place

--- src/components/UserInterface/index.js
+++ src/components/UserInterface/index.js
@@ -1,14 +1,36 @@
 // dependencies
 import { useState } from 'react';
-
 // the user focused components to be conditionally rendered
 import { Display } from './Display';
 import { Picker } from './Picker';
+// button for base case
+import { Button } from '../Button';
+// grab the data.json object and assign to a variable
+import * as DATA from '../../data.json';
 
 export const UserInterface = () => {
+    // state variables
+    // questionIndex default is null; will switch to 0 and iterate upwards with user interaction
+    const [questionIndex, setQuestionIndex] = useState(null);
+    // non-state variables
+    let content;
+    // check for truthy/falsey but cover JS 0 behavior
+    // render the Get Started button if nothing has happened or in default position
+    if (!questionIndex && questionIndex !== 0) {
+        content = <Button />
+    }
+    // render the display if user selection is complete i.e. index is does not correspond to a question
+    else if (questionIndex > DATA.questions.length - 1) {
+        content = <Display />
+    }
+    // render the picker in all other scenarios
+    else {
+        content = <Picker />
+    };
+
     return (
         <div className="interface">
-            
+            { content }
         </div>
     )
 };
